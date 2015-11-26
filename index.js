aa1e918de28f5ed066f696fb03f465324a8ac922
Olivier Beaulieu <olivier@passwordbox.com>
2015-11-26 16:48:44 -0500

    fixed issue on windows

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ var filendir = require('filendir');
 var path = require('path');
 
 module.exports = function(options){
-	options = options || {};
+    options = options || {};
 
-	return through.obj(function(row, enc, next) {
-		if (/\/node_modules\//.test(row.id)) {
-			// If it's an external dependency, copy to libs folder
-			var filename = path.join(row.id.replace(/(.+\/node_modules\/)/, ''));
-			filendir.writeFileSync(options.baseDir + filename, row.source);
+    return through.obj(function(row, enc, next) {
 
-		} else {
-			// If it's not an external dependency, proceed normally
-			this.push(row);
-		}
+        var p = path.resolve('./node_modules');
+        if (row.id.startsWith(p)) {
+            // If it's an external dependency, copy to libs folder
+            var f = path.resolve(options.baseDir, path.basename(row.id));
+            filendir.writeFileSync(f, row.source);
 
-		next();
-	});
+        } else {
+            // If it's not an external dependency, proceed normally
+            this.push(row);
+        }
+
+        next();
+    });
 };
